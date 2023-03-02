8838152d79fedbf5e880b01dcbfd510cd62c01ff
mehdi <mpourbaba@gmail.com>
2023-03-02 16:26:19 +0330

    implement order models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Order, OrderItem
+
+
+class OrdeItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product', )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'updated', 'is_paid')
+    list_filter = ('is_paid', )
+    inlines = (OrdeItemInline, )
